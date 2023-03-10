cebb5477763eb0a500d64386f6b50bfb0c3cfa60
Siddharth Chaphekar <61927942+schaphekar@users.noreply.github.com>
2023-03-10 14:01:40 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const name = "NGS";
+  const isNameShowing = false;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello </h1>
+      {name ? (
+        <>
+          <h1>test</h1>
+          <h2>There is a name.</h2>
+        </>
+      ):(
+        test
+  )}
+
     </div>
   );
 }
